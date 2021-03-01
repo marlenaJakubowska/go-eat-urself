18e29a53e757b2d3d0ffa135e997572124115ccf
marlenaJakubowska <mar.jakubowska90@gmail.com>
2021-03-01 13:08:43 +0100

    Add basic package

---
+++ src/test/java/com/marspc/goeaturself/GoEatUrselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marspc.goeaturself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoEatUrselfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
