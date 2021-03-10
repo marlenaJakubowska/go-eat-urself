b65fd0cd23ca23247a938cee5b31b8aed751897b
marlenaJakubowska <mar.jakubowska90@gmail.com>
2021-03-10 20:44:25 +0100

    Add generateJwtToken method in JWTTokenProvider class without authorization filter

---
+++ src/main/java/com/marspc/goeaturself/security/JWTTokenProvider.java
@@ -0,0 +1,42 @@
+package com.marspc.goeaturself.security;
+
+import com.auth0.jwt.JWT;
+
+import com.marspc.goeaturself.domain.UserPrincipal;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static com.marspc.goeaturself.constant.SecurityConstant.*;
+
+@Component
+public class JWTTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateJwtToken(UserPrincipal userPrincipal) {
+        String[] claims = getClaimsFromUser(userPrincipal);
+        return JWT.create()
+                .withIssuer(GO_EAT_URSELF)
+                .withAudience(GO_EAT_URSELF_ADMINISTRATION)
+                .withIssuedAt(new Date())
+                .withSubject(userPrincipal.getUsername())
+                .withArrayClaim(AUTHORITIES, claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(HMAC512(secret.getBytes()));
+    }
+
+    private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
+        List<String> authorities = new ArrayList<>();
+        for (GrantedAuthority grantedAuthority : userPrincipal.getAuthorities()) {
+            authorities.add(grantedAuthority.getAuthority());
+        }
+        return authorities.toArray(new String[0]);
+    }
+}
